7c6433fc6ec45a6dca842459ea18de54ad1812cb
Benjamin Kovacs <bach1099-btkamazonwebservices@yahoo.com>
2017-11-08 16:52:25 -0500

    changed from talon to cantalon

--- src/org/usfirst/frc/team5530/robot/subsystems/ExampleSubsystem.java
+++ src/org/usfirst/frc/team5530/robot/subsystems/ExampleSubsystem.java
@@ -3,6 +3,11 @@ package org.usfirst.frc.team5530.robot.subsystems;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
+
+
 /**
  *
  */
@@ -10,10 +15,10 @@ public class ExampleSubsystem extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 	
-	Talon left = new Talon(0);
-	Talon left2 = new Talon(1);
-	Talon right = new Talon(2);
-	Talon right2 =new Talon(3);
+	CANTalon left   = new CANTalon(0);
+	CANTalon left2  = new CANTalon(1);
+	CANTalon right  = new CANTalon(2);
+	CANTalon right2 = new CANTalon(3);
 			
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
