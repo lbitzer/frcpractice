8956f94b5cab94fed5f94da51f154a80768a9f44
Benjamin Kovacs <bach1099-btkamazonwebservices@yahoo.com>
2017-11-08 16:47:27 -0500

    added talons

--- src/org/usfirst/frc/team5530/robot/subsystems/ExampleSubsystem.java
+++ src/org/usfirst/frc/team5530/robot/subsystems/ExampleSubsystem.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team5530.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -10,7 +9,11 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class ExampleSubsystem extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
-	public Talon t1 = new Talon(0);
+	
+	Talon left = new Talon(0);
+	Talon left2 = new Talon(1);
+	Talon right = new Talon(2);
+	Talon right2 =new Talon(3);
 			
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
