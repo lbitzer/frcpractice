18b6daf7e813f5cb9176646c56ae0e076aff0a48
Benjamin Kovacs <bach1099-btkamazonwebservices@yahoo.com>
2017-11-13 16:27:02 -0500

    fixed name change

--- src/org/usfirst/frc/team5530/robot/Robot.java
+++ src/org/usfirst/frc/team5530/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team5530.robot.commands.ExampleCommand;
-import org.usfirst.frc.team5530.robot.subsystems.ExampleSubsystem;
+import org.usfirst.frc.team5530.robot.subsystems.Drivetrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -20,7 +20,7 @@ import org.usfirst.frc.team5530.robot.subsystems.ExampleSubsystem;
  */
 public class Robot extends IterativeRobot {
 
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
+	public static final Drivetrain drivetrain = new Drivetrain();
 	public static OI oi;
 
 	Command autonomousCommand;
